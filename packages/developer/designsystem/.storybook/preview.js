ba93a34566a8f7600b4f552246430e7dfda06935
gottschalk <gottschalk@intuity.de>
2021-04-15 19:01:36 +0200

    update dep

--- packages/developer/designsystem/.storybook/preview.js
+++ packages/developer/designsystem/.storybook/preview.js
@@ -1,4 +1,3 @@
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,5 +6,5 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  viewMode: 'docs'
-}
+  viewMode: "docs",
+};
