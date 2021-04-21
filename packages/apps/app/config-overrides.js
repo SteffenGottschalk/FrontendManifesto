c1ef2795033c2623965d1735472cda65de3cca1b
gottschalk <gottschalk@intuity.de>
2021-04-21 14:41:16 +0200

    remove

--- packages/apps/app/config-overrides.js
+++ packages/apps/app/config-overrides.js
@@ -3,13 +3,11 @@ const path = require('path');
 const fs = require('fs');
 
 const { override, babelInclude } = require('customize-cra');
-const rewireReactHotLoader = require('react-app-rewire-hot-loader-for-customize-cra');
 
 module.exports = (config, env) => {
   return Object.assign(
     config,
     override(
-      rewireReactHotLoader(),
       /* Makes sure Babel compiles the stuff in the common folder */
       babelInclude([
         path.resolve('src'),
